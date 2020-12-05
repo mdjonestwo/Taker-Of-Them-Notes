6d63a4c8db8c19711369fbf92759219177dc973d
mdjonestwo <mdjones2@protonmail.com>
2020-12-04 22:03:59 -0500

    Added notes.html GET response

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.get('/', function (req, res){
     res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
 })
 
+app.get('/notes', function (req, res){
+    res.sendFile(path.join(__dirname, 'Develop/public/notes.html')) 
+})
 
 app.listen(PORT, function (){
     console.log('App listening on PORT' + PORT)
